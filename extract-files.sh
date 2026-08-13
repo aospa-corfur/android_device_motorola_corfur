5416de80826a7dac580f55a819f6de9e30c2db54
Michael Bestas <mkbestas@lineageos.org>
2026-08-13 16:38:56 +0000

    corfur: Remove media_codecs_google_c2*
    AOSP codecs can be used by default and it is not necessary to include
    them device side.
    
    Change-Id: I9f072f09cb3eabee3dfeb4fcc9ef3c61397a0aa8

--- extract-files.sh
+++ extract-files.sh
@@ -60,7 +60,7 @@ function blob_fixup() {
             ;;
 
         vendor/etc/media_codecs.xml|vendor/etc/media_codecs_blair.xml|vendor/etc/media_codecs_holi.xml)
-            sed -Ei "/media_codecs_(google_audio|google_telephony|vendor_audio)/d" "${2}"
+            sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|vendor_audio)/d" "${2}"
             ;;
     esac
 }
